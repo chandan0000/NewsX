dc492f1d87c5167ed72818d02045064a6251bdf0
chandan_kumar <kumarchandan41u@gmail.com>
2021-11-02 15:08:47 +0530

    newsX

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -9,7 +10,13 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController searchController = new TextEditingController();
-  List<String> navBarItem = ["Top News" , "India" , "World" , "Finacnce" , "Health"];
+  List<String> navBarItem = [
+    "Top News",
+    "India",
+    "World",
+    "Finacnce",
+    "Health"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,110 +26,103 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
-      Container(
-      //Search Wala Container
-
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(24)),
-      child: Row(
-        children: [
-          GestureDetector(
-            onTap: () {
-              if ((searchController.text).replaceAll(" ", "") ==
-                  "") {
-                print("Blank search");
-              } else {
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
-              }
-            },
-            child: Container(
-              child: Icon(
-                Icons.search,
-                color: Colors.blueAccent,
-              ),
-              margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
-            ),
-          ),
-          Expanded(
-            child: TextField(
-              controller: searchController,
-              textInputAction: TextInputAction.search,
-              onSubmitted: (value){
-                print(value);
-              },
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Search Health"),
-            ),
-          )
-        ],
-      ),
-    ),
-
           Container(
-            height: 50,
-            child: ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: navBarItem.length,
-                itemBuilder : (context , index){
-                return InkWell(
+            //Search Wala Container
+
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(24)),
+            child: Row(
+              children: [
+                GestureDetector(
                   onTap: () {
-                    print(navBarItem[index]);
+                    if ((searchController.text).replaceAll(" ", "") == "") {
+                      print("Blank search");
+                    } else {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                    }
                   },
                   child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20 , vertical : 10),
-                    margin: EdgeInsets.symmetric(horizontal : 5),
-                    decoration : BoxDecoration(
+                    child: Icon(
+                      Icons.search,
                       color: Colors.blueAccent,
-                      borderRadius : BorderRadius.circular(15)
                     ),
-
-                    child : Center(
-                      child: Text(
-                          navBarItem[index] ,
-                      style: TextStyle(
-                        fontSize:19 ,
-                        color: Colors.white,
-                        fontWeight:FontWeight.bold
-                      )),
-                    ),
+                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (value) {
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "Search Health"),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+              height: 50,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: navBarItem.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        print(navBarItem[index]);
+                      },
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        margin: EdgeInsets.symmetric(horizontal: 5),
+                        decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Center(
+                          child: Text(navBarItem[index],
+                              style: TextStyle(
+                                  fontSize: 19,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    );
+                  })),
+          CarouselSlider(
+            options: CarouselOptions(
+                height: 200, autoPlay: true, enlargeCenterPage: true),
+            items: items.map((item) {
+              return Builder(builder: (BuildContext context) {
+                return InkWell(
+                  onTap: () {
+                    print("MUJHE MARRO MAT");
+                  },
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5, vertical: 14),
+                    child: Text(item),
                   ),
                 );
-                }
-            )
-          ),
-
-          CarouselSlider(
-            options : CarouselOptions(
-              height : 200,
-              autoPlay : true,
-                enlargeCenterPage : true
-            ),
-            items : items.map((item) {
-              return Builder(
-                builder: (BuildContext context){
-                  return InkWell(
-                    onTap: () {
-                      print("MUJHE MARRO MAT");
-                    },
-                    child: Container(
-                      margin : EdgeInsets.symmetric(horizontal : 5 , vertical : 14),
-                     child : Text(item),
-                    ),
-                  );
-                }
-              );
+              });
             }).toList(),
           ),
-
+          Container(
+            child: ListView.builder(
+                itemCount: 3,
+                 itemBuilder:(context, index) {
+                  return Container(
+                    child: Text("jbkj"),
+                  );
+                }),
+          ),
         ],
       ),
     );
   }
 
-  final List items = ["HELLO MAN" , "NAMAS STAY", "DIRTY FELLOW" ];
+  final List items = ["HELLO MAN", "NAMAS STAY", "DIRTY FELLOW"];
 }
